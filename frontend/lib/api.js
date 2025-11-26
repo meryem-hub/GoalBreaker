1222ac93d567fbc68bf720db6929a2edc77d8dfc
Meryem Ebrahim <meryemebrahim.46@gmail.com>
2025-11-26 01:05:17 -0800

    final commit

---
+++ frontend/lib/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://goalbreaker-2.onrender.com';
+
+export const goalAPI = {
+  async createGoal(goalText) {
+    const response = await axios.post(`${API_BASE_URL}/goals/`, {
+      goal_text: goalText
+    });
+    return response.data;
+  },
+
+  async getGoals() {
+    const response = await axios.get(`${API_BASE_URL}/goals/`);
+    return response.data;
+  },
+
+  async getGoalById(id) {
+    const response = await axios.get(`${API_BASE_URL}/goals/${id}`);
+    return response.data;
+  }
+};
